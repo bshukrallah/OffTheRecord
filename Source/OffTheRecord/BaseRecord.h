008e2b7de8045721a8c73c4479a9f87bdac5573d
Marcus <admin@zombietechinc.com>
2022-11-12 20:37:14 -0500

    added placeholder vinyl record and got it to spin

---
+++ Source/OffTheRecord/BaseRecord.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseRecord.generated.h"
+
+//Record status
+UENUM(BlueprintType)
+enum class ERecordStatus : uint8
+{
+	ERS_PLAYING UMETA(DisplayName = "Record Playing"),
+	ERS_STOPPED UMETA(DisplayName = "Record Stopped")
+
+};
+
+UCLASS()
+class OFFTHERECORD_API ABaseRecord : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this actor's properties
+	ABaseRecord();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	//Record Enum Status
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Items, meta = (AllowPrivateAccess = "true"))
+		ERecordStatus RecordStatus;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, Category = Movement)
+		float SpinValue;
+};
